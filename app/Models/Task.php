867c0a07bbfb25d4d29769b7dd9c6af961893d1f
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-02 20:13:06 +0330

    add sortable trait and contract

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\EloquentSortable\Sortable;
+use Spatie\EloquentSortable\SortableTrait;
 
-class Task extends Model
+class Task extends Model implements Sortable
 {
-    use HasFactory;
+    use HasFactory, SortableTrait;
 
     protected $guarded = [];
 }
