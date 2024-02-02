05dc0723b03cd79f96b9d1f2b109a63df4f245b3
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-02 21:47:46 +0330

    add count to status header

--- resources/views/vendor/filament-kanban/kanban-header.blade.php
+++ resources/views/vendor/filament-kanban/kanban-header.blade.php
@@ -1,4 +1,5 @@
 <h3 class="mb-2 px-4 font-semibold text-lg text-gray-400">
     <span class="text-primary-400">❖</span>
     {{ $status['title'] }}
+    <span class="text-sm">({{ count($status['records']) }})</span>
 </h3>
