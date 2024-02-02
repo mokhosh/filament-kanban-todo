67be2a254cb3fb072309e04509da827b73fd15bb
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-02 19:57:04 +0330

    add task model and migration

---
+++ database/migrations/2024_02_02_162551_create_tasks_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('status')->default('todo');
+            $table->timestamps();
+        });
+    }
+};
