467ad2b35101e5c48fe08efdb4f0208089043c3e
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-02 21:44:12 +0330

    add more columns to tasks table

--- database/migrations/2024_02_02_162551_create_tasks_table.php
+++ database/migrations/2024_02_02_162551_create_tasks_table.php
@@ -12,9 +12,17 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description')->nullable();
+            $table->boolean('urgent')->default(false);
+            $table->tinyInteger('progress')->default(0);
+            $table->foreignId('user_id')->constrained();
             $table->string('status')->default('Todo');
             $table->unsignedInteger('order_column');
             $table->timestamps();
+        });
+
+        Schema::create('task_user', function(Blueprint $table) {
+            $table->foreignId('task_id')->constrained();
+            $table->foreignId('user_id')->constrained();
         });
     }
 };
