4761b79fbfa6b26ffd3fbeb80e889a730f826c5e
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-02 20:14:05 +0330

    add create action

--- app/Filament/Pages/TasksKanbanBoard.php
+++ app/Filament/Pages/TasksKanbanBoard.php
@@ -4,6 +4,9 @@ namespace App\Filament\Pages;
 
 use App\Enums\TaskStatus;
 use App\Models\Task;
+use Filament\Actions\CreateAction;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Support\Collection;
 use Mokhosh\FilamentKanban\Pages\KanbanBoard;
 
@@ -30,5 +33,17 @@ class TasksKanbanBoard extends KanbanBoard
     public function onSortChanged(int $recordId, string $status, array $orderedIds): void
     {
          Task::setNewOrder($orderedIds);
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make()
+                ->model(Task::class)
+                ->form([
+                    TextInput::make('title'),
+                    Textarea::make('description'),
+                ])
+        ];
     }
 }
